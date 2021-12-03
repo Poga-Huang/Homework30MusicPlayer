52e78b9bb0913e2fdc0f249e4acb28bb7e7ee66a
Poga Huang <pgh86812@gmail.com>
2021-12-03 09:22:51 +0800

    Initial Commit

---
+++ Homework30MusicPlayerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework30MusicPlayerApp
+//
+//  Created by 黃柏嘉 on 2021/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
